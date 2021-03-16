81b50c56eb64e2b1f876674783ec5fcd7a447a17
parksangwoo1617 <b01056721617@gmail.com>
2021-03-16 17:02:14 +0900

    Express

--- routes/page.js
+++ routes/page.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
@@ -11,20 +12,31 @@ router.use((req, res, next) => {
     next();
 });
 
+router.get('/', async(req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['id', 'nick'],
+            },
+            order: [['createdAt', 'DESC']],
+        });
+        res.render('main', {
+            title: 'Express',
+            twits: posts,
+        });
+    } catch(error) {
+        console.error(err);
+        next(err);
+    }
+})
+
 router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile', { title: '내 정보 - Express' });
 });
 
 router.get('/join', isNotLoggedIn, (req, res) => {
     res.render('join', { title: '회원가입 - Express' });
-});
-
-router.get('/', (req, res, next) => {
-    const twits = [];
-    res.render('main', {
-        title: 'Express',
-        twits,
-    });
 });
 
 module.exports = router;
