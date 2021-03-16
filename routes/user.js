5936b8a9dca07f627a064d31efa8961093e18432
parksangwoo1617 <b01056721617@gmail.com>
2021-03-17 08:30:02 +0900

    Express

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { isLoggedIn } = require('./middlewares');
-const User = require('../modles/user');
+const User = require('../models/user');
 
 const router = express.Router();
 
